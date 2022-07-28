14d30da9b5ac0b418b2b6207515d2c2e8ff2519e
ThiagoAvelino1990 <thiagoavelinoalves@hotmail.com>
2022-07-28 00:43:27 -0300

    Classes e Métodos

---
+++ Calculadora.Salario/Model/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Salario.Model
+{
+    public class Funcionario
+    {   
+        public void CalculaBonus(double bonus)
+        {
+            salario = salario + bonus;
+
+        }
+        public double salario{ get; set; }
+        public string nome { get; set; }
+
+    }
+}
---
+++ Calculadora.Salario/Program.cs
@@ -0,0 +1,26 @@
+﻿//importante a classe
+using Calculadora.Salario.Model;
+
+//Instanciando a classe
+var funcionario = new Funcionario();
+
+Console.WriteLine("Favor informar o nome do funcionário: ");
+funcionario.nome = Console.ReadLine();
+
+Console.WriteLine("Favor informar o salário: ");
+funcionario.salario = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Favor informar o bonus salarial: ");
+double bonusSalario = Convert.ToDouble(Console.ReadLine());
+//Buscando o o método Calculabonus dentro da classe Funcionario
+funcionario.CalculaBonus(bonusSalario);
+
+Console.WriteLine($"O nome do funcionário é : {funcionario.nome} e o seu salário é: {funcionario.salario}");
+
+
+
+
+
+
+
+
